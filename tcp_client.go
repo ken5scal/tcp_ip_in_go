37e7779ad2e71f69558f7da2038a6e07736ba5c0
ken5scal <kengoscal@gmail.com>
2016-12-26 23:49:43 +0900

    Resolved IP address

--- tcp_client.go
+++ tcp_client.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"net"
+	"fmt"
+)
+
 var TARGET_HOST = "www.google.co.jp"
 var TARGET_PORT = "80"
 
@@ -7,6 +12,12 @@ func main() {
 	// Create TCP Socket
 
 	// Connect ot Server
+	ip, err := net.ResolveIPAddr("ip", TARGET_HOST)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(ip)
+
 
 	// Senda data
 
