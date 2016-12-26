e15e3a2554abf53f4013bb03d3a6178b220aab5c
ken5scal <kengoscal@gmail.com>
2016-12-26 23:41:18 +0900

    Add Seudo code for tcp client

--- tcp_client.go
+++ tcp_client.go
@@ -1,1 +1,14 @@
-package traffic
+package main
+
+var TARGET_HOST = "www.google.co.jp"
+var TARGET_PORT = "80"
+
+func main() {
+	// Create TCP Socket
+
+	// Connect ot Server
+
+	// Senda data
+
+	// Receive data
+}
