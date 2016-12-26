1cc6e27a83bdb7deaddbc3b07553d8b5ec2c8bde
ken5scal <kengoscal@gmail.com>
2016-12-27 00:23:56 +0900

    Finish implementing tcp client

--- tcp_client.go
+++ tcp_client.go
@@ -4,6 +4,7 @@ import (
 	"net"
 	"fmt"
 	"os"
+	"io/ioutil"
 )
 
 var TARGET_HOST = "www.google.co.jp"
@@ -22,13 +23,26 @@ func main() {
 	fmt.Println(tcpAddr.IP)
 	fmt.Println(tcpAddr.Port)
 
-	conn, err := net.Dial("tcp",tcpAddr.IP + tcpAddr.Port)
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	checkError(err)
 	defer conn.Close()
 
+	//conn.SetWriteDeadline(time.Now().Add(time.Minute))
 	// Senda data
+	//status, err := conn.Write([]byte("GET  HTTP/1.1\r\nHost: google.com\r\n\r\n"))
+	status, err := conn.Write([]byte("GET / HTTP/1.0\r\n\r\n"))
+	checkError(err)
+	fmt.Printf("status code: %v\n", string(status))
 
 	// Receive data
+	result_byte, err := ioutil.ReadAll(conn)
+	//result_byte := make([]byte, 1024)
+	//_, err = conn.Read(result_byte)
+	checkError(err)
+	//fmt.Printf("response code: %v\n", string(result))
+	fmt.Printf("response code: %v\n", string(result_byte))
+
+	os.Exit(0)
 }
 
 func checkError(err error) {
