b0d04a2dd0a3d070407ed32d5c1154d184c3ef81
ken5scal <kengoscal@gmail.com>
2016-12-27 00:27:55 +0900

    Update

--- tcp_client.go
+++ tcp_client.go
@@ -4,6 +4,7 @@ import (
 	"net"
 	"fmt"
 	"os"
+	"time"
 	"io/ioutil"
 )
 
@@ -20,24 +21,18 @@ func main() {
 	tcpAddr, err := net.ResolveTCPAddr("tcp", TARGET_HOST + ":" + TARGET_PORT)
 	checkError(err)
 
-	fmt.Println(tcpAddr.IP)
-	fmt.Println(tcpAddr.Port)
-
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	checkError(err)
 	defer conn.Close()
+	conn.SetWriteDeadline(time.Now().Add(time.Minute))
 
-	//conn.SetWriteDeadline(time.Now().Add(time.Minute))
 	// Senda data
-	//status, err := conn.Write([]byte("GET  HTTP/1.1\r\nHost: google.com\r\n\r\n"))
 	status, err := conn.Write([]byte("GET / HTTP/1.0\r\n\r\n"))
 	checkError(err)
 	fmt.Printf("status code: %v\n", string(status))
 
 	// Receive data
 	result_byte, err := ioutil.ReadAll(conn)
-	//result_byte := make([]byte, 1024)
-	//_, err = conn.Read(result_byte)
 	checkError(err)
 	//fmt.Printf("response code: %v\n", string(result))
 	fmt.Printf("response code: %v\n", string(result_byte))
