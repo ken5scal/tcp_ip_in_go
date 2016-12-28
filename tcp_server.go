7a739c8700afaaf0cbe278eb5976f9c5891f6040
ken5scal <kengoscal@gmail.com>
2016-12-28 23:49:40 +0900

    Put Seudo code

--- tcp_server.go
+++ tcp_server.go
@@ -1,7 +1,31 @@
 package main
 
-BIND_IP = "127.0.0.1"
-BIND_PORT = "80"
+import (
+	"net"
+	"fmt"
+	"os"
+)
+
+var BIND_IP = "127.0.0.1"
+var BIND_PORT = ":8080"
 
 func main() {
+	ln, err := net.Listen("tcp", BIND_PORT)
+	checkServerError(err)
+	for {
+		conn, err := ln.Accept()
+		checkServerError(err)
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(c net.Conn) {
+
+}
+
+func checkServerError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal Error: %v", err.Error())
+		os.Exit(1)
+	}
 }
