71f8eb48b675cc34bb477d0ed93809abdad264b6
ken5scal <kengoscal@gmail.com>
2016-12-29 01:03:24 +0900

    Finish implementing simple tcp-server

--- tcp_server.go
+++ tcp_server.go
@@ -4,17 +4,17 @@ import (
 	"net"
 	"fmt"
 	"os"
-	"time"
 )
 
 var BIND_IP = "127.0.0.1"
 var BIND_PORT = "8080"
 
 func main() {
-	tcpAddr, err := net.ResolveTCPAddr("tcp", BIND_IP + ":" + BIND_PORT)
-	checkServerError(err)
+	//tcpAddr, err := net.ResolveTCPAddr("tcp", BIND_IP + ":" + BIND_PORT)
+	//checkServerError(err)
 
-	ln, err := net.ListenTCP("tcp", tcpAddr)
+	ln, err := net.Listen("tcp", BIND_IP + ":" + BIND_PORT)
+	checkServerError(err)
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
@@ -27,8 +27,21 @@ func main() {
 
 func handleConnection(c net.Conn) {
 	defer c.Close()
-	daytime := time.Now().String()
-	c.Write([]byte(daytime))
+	//daytime := time.Now().String()
+	//c.Write([]byte(daytime))
+
+	var buf [512]byte
+	for {
+		n, err := c.Read(buf[0:])
+		fmt.Printf("n: %v\n", n)
+		if err != nil {
+			return
+		}
+		_, err2 := c.Write(buf[0:n])
+		if err2 != nil {
+			return
+		}
+	}
 }
 
 func checkServerError(err error) {
